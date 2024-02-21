be3045e54f6d79d98e4c5685e1094397e12f07a9
LueKely <luekely43110@gmail.com>
2024-02-21 22:01:44 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+	import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+	<RouterView></RouterView>
+</template>
+
+<style lang="scss" scoped></style>
