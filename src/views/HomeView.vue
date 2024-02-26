d06b5ff5caba427dfe5b0d459c5d1f605dc7d536
LueKely <luekely43110@gmail.com>
2024-02-26 21:01:08 +0800

    FINAL TYPOGRAPHY

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
-	<div class="test">HOME</div>
+	<div class="test-xl">
+		Hi, Im <br />
+		Lue Anunciacion
+	</div>
+	<!-- <div class="test-l">This is</div> -->
+	<div class="test-m">This is the joe biden joeee biden</div>
+	<!-- <div class="test-sm">This is the sm</div> -->
 </template>
 
 <script setup lang="ts"></script>
