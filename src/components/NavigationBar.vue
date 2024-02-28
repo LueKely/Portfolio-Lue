4caeefbd1c5ae376e66720a357fcfd77750f1cd5
LueKely <luekely43110@gmail.com>
2024-02-28 13:29:29 +0800

    Feat: navigation bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,20 @@
+<template>
+	<nav>
+		<div>
+			<p>
+				Lue <br />
+				Anunciacion
+			</p>
+		</div>
+		<ul>
+			<li><a href="#home"></a>Home</li>
+			<li><a href="#projects"></a>Projects</li>
+			<li><a href="#about"></a>About</li>
+		</ul>
+		<button>Check My CV</button>
+	</nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
