e302195f59dc5fd45ae052111514c85af22ff798
LueKely <luekely43110@gmail.com>
2024-02-29 11:06:05 +0800

    feat: tablet/mobile view

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,27 +1,37 @@
 <template>
-	<nav>
+	<nav class="desktop">
 		<div>
 			<p>
 				Lue <br />
 				Anunciacion
 			</p>
 		</div>
-		<ul>
+		<ul class="desktop">
 			<li><a href="#home">Home</a></li>
 			<li><a href="#projects">Projects</a></li>
 			<li><a href="#about">About</a></li>
 		</ul>
-		<button>Check My CV</button>
+		<button class="desktop">Check My CV</button>
+		<button class="burger">Borgar</button>
 	</nav>
+
+	<!-- mobile/table view -->
+	<aside class="burger">
+		<li><a href="#home">Home</a></li>
+		<li><a href="#projects">Projects</a></li>
+		<li><a href="#about">About</a></li>
+		<button>Check My CV</button>
+	</aside>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
-	nav {
-		@include break.phone {
-			background-color: red;
+	nav.desktop {
+		@include break.tablet {
+			// display: none;
 		}
+
 		position: sticky;
 		top: 0;
 
@@ -30,6 +40,43 @@
 		align-items: center;
 	}
 
-	ul {
+	ul.desktop {
+		@include break.tablet {
+			display: none;
+		}
+		display: flex;
+		align-items: center;
+		justify-self: center;
+
+		list-style: none;
+
+		column-gap: 10px;
+	}
+	button.burger {
+		@include break.desktop {
+			display: none;
+		}
+
+		@include break.tablet {
+			display: block;
+		}
+	}
+
+	button.desktop {
+		@include break.tablet {
+			display: none;
+		}
+		@include break.phone {
+			display: none;
+		}
+	}
+
+	aside.burger {
+		display: sticky;
+		top: 0;
+
+		@include break.desktop {
+			display: none;
+		}
 	}
 </style>
