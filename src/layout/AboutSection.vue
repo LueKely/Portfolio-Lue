31681a128e72743479be4d1f16c1f1213c3469d5
LueKely <luekely43110@gmail.com>
2024-03-25 20:42:16 +0800

    okay na to

--- src/layout/AboutSection.vue
+++ src/layout/AboutSection.vue
@@ -4,21 +4,37 @@
 			<CanvasAbout></CanvasAbout>
 		</div>
 		<div class="text-wrapper">
-			<div class="wrapper">
-				<div>
-					<h1>About Me</h1>
-				</div>
-				<div class="underline-wrapper skill-container">
-					<h2 class="skill">Skills</h2>
-				</div>
+			<div class="list">
+				<h1>About Me</h1>
+				<!-- bio -->
 				<ul>
-					<li>HTML, CSS and Javascript</li>
-					<li>JS Front & Backend Frameworks</li>
-					<li>JS libraries like Typescript</li>
-					<li>Databases such as Mongo</li>
+					<li>21 Years Old</li>
+					<li>Based In The Philippines</li>
+					<li>3rd Year College Student</li>
+				</ul>
+				<!-- skills -->
+				<ul>
+					<li>JS Frameworks</li>
+					<li>HTML, CSS and JS</li>
+					<li>Mongo DB</li>
+					<li>SQL (MySql & Postgres)</li>
+					<li>SASS & Tailwind</li>
+					<li>Typescript</li>
+					<li>Express Js</li>
 					<li>GLSL</li>
 				</ul>
 			</div>
+
+			<di class="titles">
+				<h1>a</h1>
+				<h2>Biography</h2>
+				<ul>
+					<li>a</li>
+					<li>a</li>
+					<li>a</li>
+				</ul>
+				<h2>Skills</h2>
+			</di>
 		</div>
 	</section>
 </template>
@@ -28,11 +44,53 @@
 </script>
 
 <style scoped lang="scss">
+	.titles,
+	.list {
+		height: 100%;
+		// background-color: red;
+	}
+	h1 {
+		margin-bottom: 1rem;
+	}
+	h2 {
+		font-family: var(--ff-medium);
+	}
+	.list {
+		display: flex;
+		align-items: flex-end;
+		justify-content: flex-start;
+		text-align: end;
+		flex-direction: column;
+		flex: 0.95;
+	}
+
+	.titles {
+		display: flex;
+		align-items: flex-start;
+		justify-content: flex-start;
+		flex-direction: column;
+		text-align: start;
+	}
+
+	.list > ul {
+		margin-top: calc(var(--fs-l) + 5px);
+	}
+
+	.titles > ul > li {
+		user-select: none;
+		color: transparent;
+	}
+	.titles > h1 {
+		user-select: none;
+		color: transparent;
+	}
+
 	.text-wrapper {
 		display: flex;
 		align-items: flex-start;
-		justify-content: space-between;
-		flex-direction: column;
+		justify-content: flex-start;
+		flex-direction: row;
+		text-align: end;
 		flex: 1;
 		height: 100%;
 	}
@@ -40,28 +98,15 @@
 		padding-right: 20px;
 		height: 100%;
 
-		flex: 0.8;
+		flex: 0.5;
 		display: flex;
 	}
 
-	.underline-wrapper {
-		border-top: 1px solid var(--clr-brown-200);
-		// margin-top: 20px;
-	}
-
 	ul {
-		list-style-position: inside;
-		text-indent: -2rem;
-		padding-inline: 3rem;
+		width: 100%;
+		list-style: none;
 		font-size: var(--fs-m);
-		text-align: unset;
-		font-family: var(--ff-light);
-	}
 
-	.skill {
-		margin-top: 40px;
-		margin-bottom: 1rem;
-	}
-	.skill-container {
+		font-family: var(--ff-light);
 	}
 </style>
